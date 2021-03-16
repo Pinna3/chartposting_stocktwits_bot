66e4655b836dce5c77d0b3606da6f7c127960c2e
Michael Pinna <mikepinna3@gmail.com>
2021-03-16 18:36:20 -0400

    Exhaustive list of optionable securities added to repository

---
+++ momoscreener.py
@@ -0,0 +1,10 @@
+import finnhub
+import pandas as pd
+
+#Setup client
+finnhub_client = finnhub.Client(api_key='c17qcvf48v6sj55b3t9g')
+
+# # Symbol lookup
+# print(finnhub_client.symbol_lookup('apple'))
+
+#CSV Database- comprehensive list of optionable stocks obtain from barchart
