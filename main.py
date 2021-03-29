c25fcb5f8884e6b0578b1b38f2d55374f5fb5733
Michael Pinna <mikepinna3@gmail.com>
2021-03-29 19:42:25 -0400

    main program is tweeting effectively

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ import finnhub
 from tweet import twitter_api
 import json
 import operator
+from datetime import datetime, date
+today_date = date.today().strftime('%m-%d-%y')
 
 
-
-
-def dailyscanner(json_watchlist, op_str):
+def dailyscanner(json_watchlist, op_str, publish=False):
     with open(json_watchlist) as infile:
         watchlist = json.load(infile)
 
     # Setup finnhub client connection
     finnhub_client = finnhub.Client(api_key='c1aiaan48v6v5v4gv69g')
+
     #up/down trendtrend toggle
     ops = {"<": operator.lt, ">": operator.gt}
     op_func = ops[op_str]
     opposite_ops = {">": operator.lt, "<": operator.gt}
     opposite_op_func = opposite_ops[op_str]
+
     #loop through securities and filter for up/down trends
     for index, security in enumerate(watchlist):
         try:
@@ -30,9 +32,19 @@ def dailyscanner(json_watchlist, op_str):
                     opposite_op_func(c, lower):
                     hit = {'ticker': security['ticker'], 'industry': security['industry'], 'direction': 'long'}
                     if hit not in hits:
-                        candle_object.chart(120)
                         hits.append(hit)
-                        print(hits)
+                        try:
+                            candle_object.chart(120, destination=security['mktcap'])
+                        except FileNotFoundError:
+                            security['mktcap'] = 'VeryLarge'
+                            candle_object.chart(120, destination=security['mktcap'])
+                        if publish:
+                            media = twitter_api.media_upload(f'''{security['mktcap']}Stocks/Charts/{today_date}/{security['ticker']}.png''')
+                            tweet = f'''${security['ticker']} Trade Alert\n\nType: Long, Momentum\nIndustry: {security['industry']}\nPeers: {' '.join(security['peers'])}'''
+                            twitter_api.update_status(tweet, media_ids=[media.media_id])
+                            print(hits)
+                        else:
+                            print(hits)
                 else:
                     print(security['ticker'] + ' ' + str(index) + '/' + str(len(watchlist)))
 
@@ -41,9 +53,20 @@ def dailyscanner(json_watchlist, op_str):
                     opposite_op_func(c, upper):
                     hit = {'ticker': security['ticker'], 'industry': security['industry'], 'direction': 'short'}
                     if hit not in hits:
-                        candle_object.chart(120)
                         hits.append(hit)
-                        print(hits)
+                        #Some market caps showing up null... keep an eye on
+                        try:
+                            candle_object.chart(120, destination=security['mktcap'])
+                        except FileNotFoundError:
+                            security['mktcap'] = 'VeryLarge'
+                            candle_object.chart(120, destination=security['mktcap'])
+                        if publish:
+                            media = twitter_api.media_upload(f'''{security['mktcap']}Stocks/Charts/{today_date}/{security['ticker']}.png''')
+                            tweet = f'''${security['ticker']} Trade Alert\n\nType: Short, Momentum\nIndustry: {security['industry']}\nPeers: {' '.join(security['peers'])}'''
+                            twitter_api.update_status(tweet, media_ids=[media.media_id])
+                            print(hits)
+                        else:
+                            print(hits)
                 else:
                     print(security['ticker'] + ' ' + str(index) + '/' + str(len(watchlist)))
         except:
@@ -57,32 +80,34 @@ def dailyscanner(json_watchlist, op_str):
             sectors[security['industry']] += 1
     print(sectors)
 
-# hits = []
-# while True:
-#     #longs 70%
-#     dailyscanner('MicroStocks/(5,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('SmallStocks/(5,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('MediumStocks/(5,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('MediumStocks/(30,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('MediumStocks/(20,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('MediumStocks/(25,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('LargeStocks/(5,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('LargeStocks/(20,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('LargeStocks/(30,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('LargeStocks/(25,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('VeryLargeStocks/(5,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('VeryLargeStocks/(20,)D-uptrend03-28-21.json', '>')
-#     dailyscanner('VeryLargeStocks/(30,)D-uptrend03-28-21.json', '>')
-#     #shorts 30%
-#     dailyscanner('MicroStocks/(5,)D-downtrend03-28-21.json', '<')
-#     dailyscanner('MediumStocks/(10,)D-downtrend03-28-21.json', '<')
-#     dailyscanner('LargeStocks/(10,)D-downtrend03-28-21.json', '<')
-#     dailyscanner('VeryLargeStocks/(10,)D-downtrend03-28-21.json', '<')
 
-shop = SecurityTradeData('SHOP', 365)
-shop.chart(120, destination='VeryLarge')
+hits = []
+# # while True:
+# #longs 70%
+# # dailyscanner('VeryLargeStocks/(30,)D-uptrend03-28-21.json', '>', publish=False)
+# # dailyscanner('VeryLargeStocks/(20,)D-uptrend03-28-21.json', '>')
+# dailyscanner('VeryLargeStocks/(5,)D-uptrend03-28-21.json', '>', publish=False)
+# # dailyscanner('LargeStocks/(30,)D-uptrend03-28-21.json', '>', publish=False)
+# # dailyscanner('LargeStocks/(25,)D-uptrend03-28-21.json', '>')
+# # dailyscanner('LargeStocks/(20,)D-uptrend03-28-21.json', '>')
+# # dailyscanner('LargeStocks/(5,)D-uptrend03-28-21.json', '>', publish=False)
+# dailyscanner('MediumStocks/(30,)D-uptrend03-28-21.json', '>', publish=False)
+# # dailyscanner('MediumStocks/(25,)D-uptrend03-28-21.json', '>')
+# # dailyscanner('MediumStocks/(20,)D-uptrend03-28-21.json', '>')
+# # dailyscanner('MediumStocks/(5,)D-uptrend03-28-21.json', '>', publish=False)
+# dailyscanner('SmallStocks/(5,)D-uptrend03-28-21.json', '>', publish=False)
+# dailyscanner('MicroStocks/(5,)D-uptrend03-28-21.json', '>', publish=False)
 
-media = twitter_api.media_upload('VeryLargeStocks/Charts/03-29-21/SHOP.png')
-tweet = '$SHOP Below 200SMA'
+#shorts 30%
+# dailyscanner('VeryLargeStocks/(10,)D-downtrend03-28-21.json', '<', publish=True)
+dailyscanner('LargeStocks/(10,)D-downtrend03-28-21.json', '<', publish=True)
+dailyscanner('MediumStocks/(10,)D-downtrend03-28-21.json', '<', publish=True)
+dailyscanner('MicroStocks/(5,)D-downtrend03-28-21.json', '<', publish=True)
 
-twitter_api.update_status(tweet, media_ids=[media.media_id])
+# shop = SecurityTradeData('SHOP', 365)
+# shop.chart(120, destination='VeryLarge')
+#
+# media = twitter_api.media_upload('VeryLargeStocks/Charts/03-29-21/SHOP.png')
+# tweet = '$SHOP Below 200SMA'
+#
+# twitter_api.update_status(tweet, media_ids=[media.media_id])
