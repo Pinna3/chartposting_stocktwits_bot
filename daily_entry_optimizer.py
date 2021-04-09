1c3dce1173ca6914e463da803c210ea34b0e8d43
Michael Pinna <mikepinna3@gmail.com>
2021-04-09 19:52:15 -0400

    daily_entry_optimizer in progress

--- daily_entry_optimizer.py
+++ daily_entry_optimizer.py
@@ -1,18 +1,25 @@
 import json
 import pandas as pd
+from risk_parameter import preset_daily_counter_capacities
+from utility_func import pull_top_tier_unbroken_trenders
 
-trender_watchlist_filename = 'MicroStocks/Watchlists/(25,)D-uptrend.json'
+trender_watchlist_filenames = pull_top_tier_unbroken_trenders(tier_percentage=10)
 
-#get dataframes for trender  watchlist
-with open(trender_watchlist_filename) as infile:
-    watchlist = json.load(infile)
-    hits = []
-    for stock in watchlist:
-        ticker = stock['ticker']
-        df = pd.read_csv(f"MicroStocks/Dataframes/{ticker}.csv", index_col=0)
-        c, h, l, o, v, sma9, sma20, sma50, sma200, lower, upper, atr = df.iloc[-1]
-        #if op_str = '>' (uptrend):
-        if sma9 > sma20 and sma20 > sma50 and sma50 > sma200 and c < lower:
-            hits.append(ticker)
+hits = []
+for filename in trender_watchlist_filenames:
+    if filename is not None:
+        with open(filename[0]) as infile:
+            watchlist = json.load(infile)
+            for stock in watchlist:
+                ticker = stock['ticker']
+                mktcap = stock['mktcap']
+                df = pd.read_csv(f"{mktcap}Stocks/Dataframes/{ticker}.csv", index_col=0)
+                c, h, l, o, v, sma9, sma20, sma50, sma200, lower, upper, atr = df.iloc[-1]
+                #if op_str = '>' (uptrend):
+                if sma9 > sma20 and sma20 > sma50 and sma50 > sma200 and l < lower:
+                    hits.append(ticker)
 
-print(print(hits))
+print(len(hits))
+
+###Might be easier to just set up the scanner to adjust ror_prioritization_factor based on results of the daily counter. (more robust data that way because
+### we're capturing the lows of the day rather than just the close, do high / low instead)
--- risk_parameter.py
+++ risk_parameter.py
@@ -115,8 +115,6 @@ Long Max Exposure: {long_max_exposure}%, Short Max Exposure: {short_max_exposure
         else:
             return False
 
-print(check_daily_counter_capacity('long', 'Medium'))
-
 #Checks to make sure date is current, if so adds to daily tally.
 #If its the first trade of the day it erases yesterday tally and starts fresh.
 def add_to_daily_counter(side, mkt_cap):
